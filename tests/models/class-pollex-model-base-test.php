0070a04a6b5dccbe627318031bd4a7d5f5ef268c
Tim van Osch <timvosch@pollex.nl>
2019-02-19 18:15:12 +0100

    Add new test

--- tests/models/class-pollex-model-base-test.php
+++ tests/models/class-pollex-model-base-test.php
@@ -15,7 +15,7 @@ final class Test_Model extends Pollex_Model_Base {
 }
 
 final class Pollex_Model_Base_Test extends TestCase {
-    public function testCanConstructFromArray() {
+    public function testShouldConstructFromArray() {
         // Arrange
         $row = array(
             'id' => 0,
@@ -23,7 +23,6 @@ final class Pollex_Model_Base_Test extends TestCase {
         );
         // Act
         $model = new Test_Model($row);
-        // Assert
         // Assert instance
         $this->assertInstanceOf(
             Pollex_Model_Base::class,
@@ -38,5 +37,54 @@ final class Pollex_Model_Base_Test extends TestCase {
             $model->name,
             'Terry'
         );
+    }
+
+    public function testShouldReturnArrayOnWhere() {
+        // Arrange
+        $rows = array(
+            array(
+                'id' => 0,
+                'name' => 'Terry'
+            ),
+            array(
+                'id' => 1,
+                'name' => 'Jake'
+            ),
+            array(
+                'id' => 2,
+                'name' => 'Amy'
+            ),
+        );
+        global $wpdb;
+        $wpdb = $this->getMockBuilder(stdClass::class)
+            ->setMockClassName('wpdb')
+            ->setMethods(['get_results', 'prepare'])
+            ->getMock();
+        $wpdb->method('get_results')->willReturn($rows);
+        $wpdb->method('prepare')->willReturn('');
+        // Act
+        $result = Test_Model::where('');
+        
+        // Assert for each model returned
+        for ($i=0; $i < 3; $i++) { 
+            // Assign current model
+            $model = $result[$i];
+            $actual = $rows[$i];
+            // Should be an instance of Test_Model
+            $this->assertInstanceOf(
+                Test_Model::class,
+                $model
+            );
+            // Should have a correct Id
+            $this->assertEquals(
+                $actual['id'],
+                $model->id
+            );
+            // Should have a name
+            $this->assertEquals(
+                $actual['name'],
+                $model->name
+            );
+        }
     }
 }
