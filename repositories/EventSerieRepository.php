56422ad95361a20ba1a29aa156d0f52d659cd138
Tim van Osch <timvosch@pollex.nl>
2019-03-27 21:34:08 +0100

    Throw entity not found exception

--- repositories/EventSerieRepository.php
+++ repositories/EventSerieRepository.php
@@ -39,7 +39,7 @@ class EventSerieRepository {
         $result = $wpdb->get_row($query, ARRAY_A);
         // Check existance
         if ($result == null) {
-            throw new \Exception(sprintf('EventSerie with id %d does not exist', $id));
+            throw EntityNotFoundException::create_from_entity_and_id('EventSerie', $id);
         }
         // Return
         return (new EventSerieFactory())->from_array($result)->create();
