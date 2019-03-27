0cbf0f3c43d9d90f630d9e6e0143783d422ff79e
Tim van Osch <timvosch@pollex.nl>
2019-03-27 11:11:58 +0100

    Allow base to be empty

--- api/Controller.php
+++ api/Controller.php
@@ -5,13 +5,17 @@ class Controller extends \WP_REST_Controller {
     protected $namespace;
     protected $base;
 
-    public function __construct($namespace, $base) {
+    public function __construct($namespace, $base = '') {
         $this->namespace = $namespace;
         $this->base = $base;
     }
 
     public function get_full_url() {
-        return $this->namespace . '/' . $this->base;
+        $full_url = $this->namespace;
+        if (!empty($this->base)) {
+            $full_url .= '/' . $this->base;
+        }
+        return $full_url;
     }
 
 }
