683947ebb5502feeadd2ddf281b29714de592176
Tim van Osch <timvosch@pollex.nl>
2019-03-27 21:33:23 +0100

    Fix base  Exception namespace

--- exceptions/EntityNotFoundException.php
+++ exceptions/EntityNotFoundException.php
@@ -1,6 +1,6 @@
 <?php namespace Pollex\Calendar\Exceptions;
 
-class EntityNotFoundException extends Exception {
+class EntityNotFoundException extends \Exception {
     public static function create_from_entity_and_id(string $entity, int $id) {
         return new static(sprintf(
             '%s with id %d does not exist',
