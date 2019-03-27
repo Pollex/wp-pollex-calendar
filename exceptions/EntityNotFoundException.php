b01927d8284b3cf8500d7a878b3b3e6750f2e2e7
Tim van Osch <timvosch@pollex.nl>
2019-03-27 21:31:06 +0100

    Add exception

---
+++ exceptions/EntityNotFoundException.php
@@ -0,0 +1,11 @@
+<?php namespace Pollex\Calendar\Exceptions;
+
+class EntityNotFoundException extends Exception {
+    public static function create_from_entity_and_id(string $entity, int $id) {
+        return new static(sprintf(
+            '%s with id %d does not exist',
+            $entity,
+            $id
+        ));
+    }
+}
