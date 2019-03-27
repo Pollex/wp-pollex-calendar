7c3c7d3d0c419ccd896585a9fc30934535a3085e
Tim van Osch <timvosch@pollex.nl>
2019-03-27 21:00:47 +0100

    Implement get all events endpoint

--- api/EventsController.php
+++ api/EventsController.php
@@ -1,5 +1,7 @@
 <?php namespace Pollex\Calendar\API;
 
+use Pollex\Calendar\Repositories\EventRepository as EventRepository;
+
 class EventsController extends Controller{
 
     public function register_routes() {
@@ -13,13 +15,29 @@ class EventsController extends Controller{
                 'methods' => \WP_REST_Server::READABLE,
                 'callback' => array( $this, 'get_events' ),
                 'permission_callback' => array( $this, 'get_events_permission_check' ),
-                'args' => array( )
+                'args' => array(
+                    'start' => array(
+                        'required' => true,
+                        'validate_callback' => 'rest_parse_date'
+                    ),
+                    'end' => array(
+                        'required' => true,
+                        'validate_callback' => 'rest_parse_date'
+                    )
+                )
             )
         ));
     }
 
     public function get_events( $request ) {
-        return new \WP_Rest_Response( 'Hello world', 200 );
+        // Parse start and end datetimes
+        $start = new \DateTime($request->get_param('start'));
+        $end = new \DateTime($request->get_param('end'));
+        // Create a repository and request the data
+        $repo = new EventRepository();
+        $data = $repo->find_all_in_period($start, $end);
+        // Respond with data
+        return new \WP_Rest_Response( $data, 200 );
     }
 
     public function get_events_permission_check( $request ) {
